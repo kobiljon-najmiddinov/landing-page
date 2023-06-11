84916be7180a5152a7f6d3dc26a51f3cb9651ffd
Kobiljon <qobiljonnajmiddinov@gmail.com>
2023-06-11 22:41:24 +0500

    section links

--- js/app.js
+++ js/app.js
@@ -4,25 +4,29 @@ const navList = document.getElementById('navbar__list');
 
 const sections = document.getElementsByTagName('section'); //"sections" variable to count the overall sections
 
+const listOfClasses = navList.classList;
+console.log(sections);
+
 //"Counting Dynamic values of Sections" e.g if there are 3 sections there will be 3 section on navigation, 
 // if added one more section then 4 sections will be visible on nav
-function getSections(sections) {
-    //Looping over and printing data-nav information
-    //using dataset property to read/write data attribute(data-*)
-    for (const sectionData of sections) {
 
-        const navData = sectionData.dataset.nav;
+for (const sectionData of sections) {
+    const sectionId = sectionData.id; //Id of section tag to link anchor, Ex. id with "section1"
+    const navData = sectionData.dataset.nav;
 
-        navList.insertAdjacentHTML("afterbegin", `<li>${navData}</li>`);
-
-    }
+    getSections(sectionId, navData);
 
 }
 
-getSections(sections);
+function getSections(sectionId, navData) {
+    navList.innerHTML += `<a href="#${sectionId}" class="menu__link"><li>${navData}</li></a>`;
+}
 
-//Navigation creation
 
 
+navList.addEventListener("click", (e) => {
+    console.log(listOfClasses);
+
+})
 
 
