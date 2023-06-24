47b3d59f110569ebeb9e5982ce19575ec4f30969
Kobiljon <qobiljonnajmiddinov@gmail.com>
2023-06-24 17:49:38 +0500

    created dynamic section array, fixed active class viewport

--- js/app.js
+++ js/app.js
@@ -2,100 +2,115 @@
 const navMenu = document.getElementsByClassName('navbar__menu');
 const navList = document.getElementById('navbar__list');
 
-const sections = document.getElementsByTagName('section'); //"sections" variable to count the overall sections
+const sections = document.querySelectorAll('section'); //"sections" variable to count the overall sections
 
-const listOfClasses = sections.classList;
-console.log(sections);
+// Store section id, title, link
+//Manually storing information
+// const sectionInfo = [
+//     {id: 'section1', name: 'Section 1', link: '#section1'},
+//     {id: 'section2', name: 'Section 2', link: '#section2'},
+//     {id: 'section3', name: 'Section 3', link: '#section3'},
+//     {id: 'section4', name: 'Section 4', link: '#section4'}
+// ];
+
+// Dynamically storing the section information
+
+let section = []; //array to store information
+// Listening the Changes that wil be made to DOM
+
+document.addEventListener('DOMContentLoaded', dynamicallyUpdateSection);
+document.addEventListener('DOMNodeInserted', dynamicallyUpdateSection);
+document.addEventListener('DOMNodeRemoved', dynamicallyUpdateSection);
+
+function dynamicallyUpdateSection(){
+    section = Array.from(document.querySelectorAll('section')).map((section)=>({
+        id: section.id,
+        name: section.dataset.nav,
+        link: `#${section.id}`
+    }));
+     
+}
+dynamicallyUpdateSection(); //Calling the function
+console.log(section);
+
 
 //"Counting Dynamic values of Sections" e.g if there are 3 sections there will be 3 section on navigation, 
 // if added one more section then 4 sections will be visible on nav
 
-for (const sectionData of sections) {
-    const sectionId = sectionData.id; //Id of section tag to link anchor, Ex. id with "section1"
-    const navData = sectionData.dataset.nav;
-
-    const listofClass = sectionData.classList;
-    // console.log(listofClass);
-
-    getSections(sectionId, navData, listofClass);
+for(const sectionIndex of section){
+    const navItems = document.createElement('li');
+    navItems.innerHTML = `<a href="${sectionIndex.link}" id="${sectionIndex.id}" class="menu__link">${sectionIndex.name}</a>`;
+    navList.appendChild(navItems);
 
 }
 
-function getSections(sectionId, navData, listofClass) {
-    //Dynamicly adding Sections
-    navList.innerHTML += `<a href="#${sectionId}" class="menu__link"><li>${navData}</li></a>`;
-
-    //Distinguishing the Sections with active state while on viewport
-    const sectionView = document.getElementById(`${sectionId}`);
+ // Distinguishing the Sections with active state while on viewport
+ document.addEventListener('scroll', function(){
+     for(const numOfSection of sections){    
+        const rect = numOfSection.getBoundingClientRect();
+        const rectTop = rect.top.toFixed();
+        console.log(rect);
     
-    document.addEventListener('scroll', function(){
-        const rectTop = sectionView.getBoundingClientRect().top.toFixed();
-        // console.log(rectTop);
         if(rectTop <= 35 &&  rectTop >= 0){
             // console.log(listofClass);
-            listofClass.add('active-class');
+            numOfSection.classList.add('active-class');
             
         }
         else if(rectTop <= -77){
-            listofClass.remove('active-class');
+            numOfSection.classList.remove('active-class');
         }
-         else if(rectTop >= 200 | rectTop <=-77 && listofClass.contains('active-class') == true){
-             listofClass.remove('active-class');
+         else if(rectTop >= 200 | rectTop <=-77 && numOfSection.classList.contains('active-class') == true){
+             numOfSection.classList.remove('active-class');
          }
-    })
-    
-    navList.addEventListener('click', function(event){
-        event.preventDefault();
-        const links = document.querySelector('a').href;
-        console.log(links);
-        links.scrollTo({
-            top:100,
-            bottom: 100,
-            bahavior: 'smooth'
-        });
-    })
-    
-    
-    
-    
-}
+         
+        
+     }
+})
 
-
-// const sectionWithQuery = document.querySelectorAll('section');
-// console.log(sectionWithQuery);
 
 // Scroll to Section effect
-// const links = document.querySelectorAll('a');
 
-// for(const index of links){
-//     const linkElem = index;
-//     handleDefault(linkElem);
+navList.addEventListener('click', function(event){
+    console.log(event);
+    // event.preventDefault();
+    console.log(event.target.id);
+   
+    // const section = document.getElementById("section1");
+    // console.log(section);
+    // const selected = document.getElementById(`${sectionIndex.id}`);
+    const selected = document.querySelector(`section#${event.target.id}`);
 
-// }
+    const linkRect = selected.getBoundingClientRect();
+    console.log(linkRect);
+    console.log(selected);
+    selected.scrollTo({
+        top: `${linkRect.top.toFixed()}`,
+        left: `${linkRect.bottom.toFixed()}`,
+        behavior: 'smooth'
+    });
+     
+})
+
+// navList.addEventListener('click', function(event){
+//     console.log(event);
+//     event.preventDefault();
+//     console.log(event.target.id);
+   
+//     // const section = document.getElementById("section1");
+//     // console.log(section);
+//     const selected = document.getElementById(`${}`);
+//     const linkRect = selected.getBoundingClientRect();
+//     console.log(linkRect);
+//     console.log(selected);
+//     selected.scrollTo({
+//         top: 0,
+//         left: 0,
+//         behavior: 'smooth'
+//     });
+     
+// })
 
 
 
-// function handleDefault(linkElem){
-//     const linkRect = linkElem.getBoundingClientRect();
-//     // console.log(linkRect);
-//     linkElem.addEventListener('click', function(event){
-//         linkElem.scroll({top: 100,
-//         left: 100,
-//     behavior: "smooth"});
-//         event.preventDefault();
-//     })
-    
-
-// }
 
 
-
-// linkID.addEventListener("click", handleClick);
-
-// function handleClick(e){
-//     e.preventDefault();
-
-
-
-
-// }
